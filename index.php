77d99e1f746429a8c56461c3fc2b34dff2397372
Nemolite <g25092011@mail.ru>
2023-06-07 08:37:55 +0300

    start

---
+++ index.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+<body>
+<header class="header">
+    <div class="header-main">
+        <div class="headre-logo">
+            <a href="/">
+                <img src="images/logo.png" alt="Логотип">
+            </a>
+        </div>
+        <div class="header-menu">
+            <nav>
+                <ul>
+                    <li><a href="">Главная</a></li>
+                    <li><a href="">Корзина</a></li>
+                    <li><a href="">Аккаунт</a></li>
+                    <li><a href="">О нас</a></li>
+                    <li><a href="">Наши контакты</a></li>
+
+                </ul>
+            </nav>
+        </div>
+        <div class="header-login">
+            <p><a href="">Регистрация</a> / <a href="">Вход</a></p>
+        </div>
+    </div>
+</header>
+
+<div class="main">
+    <div class="main-sidebar">
+
+    </div>
+    <div class="main-content">
+
+    </div>
+</div>
+<footer class="footer">
+    <div class="footer-main">
+        <div class="footer-left">
+
+            <a href="">Админка</a></p>
+
+        </div>
+        <div class="footer-mid"></div>
+        <div class="footer-right"></div>
+    </div>
+</footer>
+<script src="js/script.js"></script>
+</body>
+</html>
