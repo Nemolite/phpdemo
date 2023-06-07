ce62a61e55a89b2f7fadf57619137863a7a8ca66
Nemolite <g25092011@mail.ru>
2023-06-07 09:51:06 +0300

    show category&product

--- footer.php
+++ footer.php
@@ -1,1 +1,14 @@
-<?php
+<footer class="footer">
+    <div class="footer-main">
+        <div class="footer-left">
+
+            <a href="">Админка</a></p>
+
+        </div>
+        <div class="footer-mid"></div>
+        <div class="footer-right"></div>
+    </div>
+</footer>
+<script src="js/script.js"></script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,58 +1,38 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Document</title>
-</head>
-<body>
-<header class="header">
-    <div class="header-main">
-        <div class="headre-logo">
-            <a href="/">
-                <img src="images/logo.png" alt="Логотип">
-            </a>
-        </div>
-        <div class="header-menu">
-            <nav>
-                <ul>
-                    <li><a href="">Главная</a></li>
-                    <li><a href="">Корзина</a></li>
-                    <li><a href="">Аккаунт</a></li>
-                    <li><a href="">О нас</a></li>
-                    <li><a href="">Наши контакты</a></li>
-
-                </ul>
-            </nav>
-        </div>
-        <div class="header-login">
-            <p><a href="">Регистрация</a> / <a href="">Вход</a></p>
-        </div>
-    </div>
-</header>
+<?php include_once __DIR__ . '/header.php'; ?>
 
 <div class="main">
     <div class="main-sidebar">
-
+        <h2>Категории товаров</h2>
+        <?php
+        $pdo->exec("set names utf8");
+        $sql = "SELECT * FROM categories";
+        $result = $pdo->query($sql);
+        while($category = $result->fetch()){
+        ?>
+            <a href=""><h3><?php echo $category['name'];?></h3></a>
+            <p><?php echo $category['description'];?></p>
+        <?
+        }
+        ?>
     </div>
     <div class="main-content">
+        <h2>Каталог товаров</h2>
+        <?php
+        $pdo->exec("set names utf8");
+        $sql = "SELECT * FROM products";
+        $result = $pdo->query($sql);
+        while($product = $result->fetch()){
+            ?>
+            <h3><?php echo $product['name'];?></h3>
+            <p><?php echo $product['description'];?></p>
+            <p><?php echo $product['description'];?></p>
+            <p><?php echo $product['price'];?></p>
+            <p><?php echo $product['country'];?></p>
+            <?
+        }
+        ?>
 
     </div>
 </div>
-<footer class="footer">
-    <div class="footer-main">
-        <div class="footer-left">
 
-            <a href="">Админка</a></p>
-
-        </div>
-        <div class="footer-mid"></div>
-        <div class="footer-right"></div>
-    </div>
-</footer>
-<script src="js/script.js"></script>
-</body>
-</html>
+<?php include_once __DIR__ . '/footer.php'; ?>
