1fbb2d87c4e716e69d630da531d96dd306e95af6
Nemolite <g25092011@mail.ru>
2023-06-08 10:27:27 +0300

    insert users

---
+++ register.php
@@ -0,0 +1,30 @@
+<?php include_once __DIR__ . '/header.php'; ?>
+<div class="main">
+    <div class="main-register">
+        <form method="post" action="" id="registerform" name="registerform">
+                <legend>Регистрация нового пользователя</legend>
+                <div class="mb-3">
+                    <label for="name" class="form-label">Имя пользователя</label>
+                    <input type="text" id="name" name="name" class="form-control" placeholder="Имя пользователя" required>
+                </div>
+                <div class="mb-3">
+                    <label for="email" class="form-label">E-mail</label>
+                    <input type="text" id="email" name="email" class="form-control" placeholder="Электрнная почта" required>
+                </div>
+                <div class="mb-3">
+                    <label for="pass1" class="form-label">Пароль</label>
+                    <input type="password" id="pass1" name="pass1" class="form-control" required>
+                </div>
+                <div class="mb-3">
+                    <label for="pass2" class="form-label">Пароль</label>
+                    <input type="password" id="pass2" name="pass2" class="form-control" required>
+                </div>
+                <button type="submit" id="register" name="register" class="btn btn-primary">Зарегистрировать</button>
+
+        </form>
+        <p id="register-msg"></p>
+        <?php registerUsers($pdo); ?>
+        <?php dd($_POST);?>
+    </div>
+</div>
+<?php include_once __DIR__ . '/footer.php'; ?>
